759f234da2ea228eb3a7417750fba4b5ef6923e7
Taylo <Taylo@MARIEL>
2025-04-25 11:40:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static bool IfYearIsLeap(int year)
+    {
+        if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+        {
+            return true; // It's a leap year!
+        }
+        else
+        {
+            return false; // Not a leap year
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Test the function
+        Console.WriteLine("Is 2016 a leap year? " + IfYearIsLeap(2016)); // true
+        Console.WriteLine("Is 2018 a leap year? " + IfYearIsLeap(2018)); // false
+    }
+}
